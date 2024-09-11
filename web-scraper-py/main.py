5fa3e0dd96a81c99e568d29dd7aaa6efb6dc4d1f
AlxLazar <alexandru.laz@gmail.com>
2024-09-11 16:42:40 +0300

    WS-003 Fixed headless mode issue

--- web-scraper-py/main.py
+++ web-scraper-py/main.py
@@ -2,7 +2,11 @@ import json
 import time
 
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
 
 with open('/home/alxlazar/Projects/alxlazar-personal-projects-core-dev/web-scraper-py/site_db.json') as data_file:
     data = json.load(data_file)
@@ -13,22 +17,30 @@ with open('/home/alxlazar/Projects/alxlazar-personal-projects-core-dev/web-scrap
         minPrice = 100.0
         for siteName, siteData in productData.items():
             print(f"  {siteName}")
-            options = FirefoxOptions()
+            # Setup Firefox options and service
+            options = Options()
             options.add_argument("-headless")
             options.add_argument('--disable-blink-features=AutomationControlled')
             with webdriver.Firefox(options=options) as driver:
-                driver.implicitly_wait(5)
-                driver.get(siteData['url'])
+                try:
+                    # driver.implicitly_wait(5)
+                    driver.get(siteData['url'])
+                    WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.TAG_NAME, "h1"))
+                    )
                 
-            path = siteData['price_xpath']
-            priceTemp = driver.find_element('xpath', path)
-            time.sleep(2)
-            price = eval(siteData['price'])
+                    path = siteData['price_xpath']
+                    priceTemp = driver.find_element('xpath', path)
+                    # time.sleep(5)
+                    price = eval(siteData['price'])
 
-            if(float(price) < float(minPrice)):
-                minPrice = price
-                minItem = siteName
-            print(f"    {price}")
+                    if(float(price) < float(minPrice)):
+                        minPrice = price
+                        minItem = siteName
+                    print(f"    {price}")
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                finally:
+                    driver.quit()
 
-            driver.quit()
         print(f"Best price for {productKey} is {minPrice} lei on {minItem}")
